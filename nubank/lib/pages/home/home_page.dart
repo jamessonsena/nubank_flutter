da8ddd7e3628e5ba9f930e8d86551f7829ee8716
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-06-05 21:49:35 -0300

    inicio da criação da tela home

--- nubank/lib/pages/home/home_page.dart
+++ nubank/lib/pages/home/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:nubank/pages/home/widgets/my_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showMenu;
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.purpleAccent,
+      body: Stack(
+        children: <Widget>[
+        MyAppBar(showMenu: _showMenu,
+        onTap: (){
+          _showMenu = !_showMenu;
+        },)
+      ],)
     );
   }
 }
---
+++ nubank/lib/pages/home/widgets/my_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+  final VoidCallback onTap;
+
+  const MyAppBar({Key key, this.showMenu, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(     
+        children: <Widget>[
+          SizedBox(
+            height: MediaQuery.of(context).padding.top,
+          ),
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              color: Colors.red,
+              height: 200,
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[                
+                      Image.network('https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-3.png',
+                      height: 35,
+                      color: Colors.white,),
+                      SizedBox(
+                        width: 10, 
+                      ),
+                      Text("Jamesson",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),)
+                    ],
+                  ),
+               Icon(showMenu ? Icons.expand_more :Icons.expand_less )
+                ],
+              ),
+            ),
+          )
+        ],
+      
+    );
+  }
+}
